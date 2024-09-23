c231c35e50262d44754904aaae4e13ab288e5e6d
Dreems <m.alexandre.delpy@gmail.com>
2024-09-23 14:50:34 +0200

    css and js enqueue

--- fuctions.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Theme fuctions 
- * 
- * @package Nevada
- */
--- header.php
+++ header.php
@@ -9,11 +9,13 @@
 <!DOCTYPE html>
 <html lang="<?php language_attributes(  ); ?>">
 <head>
-    <meta charset="<php bloginfo('charset');?>">
+    <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+    
+<?php wp_body_open(); ?>
 
 <header>
     Header
