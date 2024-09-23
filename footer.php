2f41d6c64b9b50ac92b1455fd98313414006bda4
Dreems <m.alexandre.delpy@gmail.com>
2024-09-23 14:19:06 +0200

    Admin bar

--- footer.php
+++ footer.php
@@ -6,6 +6,11 @@
  * @package Aquila
 */
 ?>
+
 <footer>
     Footer
 </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -6,6 +6,15 @@
  * @package Aquila
 */
 ?>
+<!DOCTYPE html>
+<html lang="<?php language_attributes(  ); ?>">
+<head>
+    <meta charset="<php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head();?>
+</head>
+<body>
+
 <header>
     Header
 </header>
